fd6c780f634d4dd19aa457978f7fd48107bdbb07
Basil Shubin <basil.shubin@gmail.com>
2020-10-12 10:37:54 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,57 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import codecs
+import os
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+# When creating the sdist, make sure the django.mo file also exists:
+if "sdist" in sys.argv or "develop" in sys.argv:
+    os.chdir("addthis")
+    try:
+        from django.core import management
+
+        management.call_command("compilemessages", stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        if "sdist" in sys.argv:
+            raise
+    finally:
+        os.chdir("..")
+
+
+def read(*parts):
+    file_path = os.path.join(os.path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name="django-addthis",
-    version="3.0.0",
+    version=find_version("addthis", "__init__.py"),
+    license="GPLv3",
+    install_requires=[
+        "Django (>=1.11.0)",
+    ],
+    description="Simple integration of the AddThis social sharing widget for Django projects.",
+    long_description=read("README.rst"),
     author="Raymond Wanyoike",
     author_email="raymond.wanyoike@gmail.com",
-    url="https://github.com/raymondwanyoike/django-addthis/",
-    description="Simple integration of the AddThis social sharing widget " +
-        "for Django projects.",
-    long_description=open("README.rst").read(),
-    download_url="https://github.com/raymondwanyoike/django-addthis",
-    license="GPLv3",
-    packages=find_packages(),
+    maintainer="Basil Shubin",
+    maintainer_email="basil.shubin@gmail.com",
+    url="https://github.com/bashu/django-addthis/",
+    download_url="https://github.com/bashu/django-addthis/zipball/master",
+    packages=find_packages(exclude=("example*", "*.tests*")),
     include_package_data=True,
-    keywords="django, templatetag, addthis, sharing",
-    install_requires=[
-        "Django>=1.4",
-    ],
+    zip_safe=False,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
@@ -28,9 +60,12 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    zip_safe=False,
 )
